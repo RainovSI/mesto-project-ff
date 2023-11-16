5cd87138d866b6a7dc1461d85dd796ddcc91fa5e
Stanislav Rainov <stanislav.rainov@yandex.ru>
2023-11-16 18:28:26 +0300

    Исправления

--- scripts/index.js
+++ scripts/index.js
@@ -1,24 +1,24 @@
-const cardTemplate = document.querySelector("#card-template").content;
-const cardList = document.querySelector(".places__list");
+const cardTemplate = document.querySelector('#card-template').content;
+const cardList = document.querySelector('.places__list');
 
 function createCard(title, image, callback) {
-  const cardElement = cardTemplate.querySelector(".card").cloneNode(true);
+  const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
 
-  cardElement.querySelector(".card__description").textContent = title;
-  const cardImage = cardElement.querySelector(".card__image");
+  cardElement.querySelector('.card__title').textContent = title;
+  const cardImage = cardElement.querySelector('.card__image');
   cardImage.alt = title;
   cardImage.src = image;
 
-  const deleteButton = cardElement.querySelector(".card__delete-button");
-  deleteButton.addEventListener("click", callback);
+  const deleteButton = cardElement.querySelector('.card__delete-button');
+  deleteButton.addEventListener('click', callback);
 
   return cardElement;
 }
 
-const deleteCard = (event) => {
-  const cardListItem = event.target.closest(".card");
+function deleteCard(event) {
+  const cardListItem = event.target.closest('.card');
   cardListItem.remove();
-};
+}
 
 function addCard(item) {
   cardList.append(item);
@@ -27,4 +27,4 @@ function addCard(item) {
 initialCards.forEach((item) => {
   const cardData = createCard(item.name, item.link, deleteCard);
   addCard(cardData);
-});
+})
